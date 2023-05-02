83ed9e025fc9edbe17dbd22c64c786e7281e7c62
ibtisam148 <95739171+ibtisam148@users.noreply.github.com>
2023-05-02 19:23:51 +0500

    Update King.java

--- King.java
+++ King.java
@@ -2,20 +2,48 @@ public class King extends Piece {
     public King(Colour colour) {
         super(colour);
 
-        if (colour == Colour.WHITE){
+        if (colour == Colour.WHITE) {
             name = 'K';
-        }else{
+        } else {
             name = 'k';
         }
     }
 
-    @Override
-    public boolean Movement() {
-        return false;
-    }
 
     @Override
-    public boolean Validation() {
-        return false;
+    public boolean MovementValidation(Selected selected, Moving moving, Moving[] list, Piece board[][]) {
+
+        boolean flag = false;
+        int i, c;
+
+        for (i = 0; i < list.length; i++) {
+            if ((list[i].rankMove == moving.rankMove) && (list[i].fileMove == moving.fileMove)) {
+                flag = true;
+            }
+        }
+        if (!flag) {
+            System.out.print("Invalid move.\n");
+            return false;
+        }
+        if (moving.fileMove == selected.fileSelect + 1) {
+            return true;
+        } else if (moving.fileMove == selected.fileSelect - 1) {
+            return true;
+        } else if (moving.rankMove == selected.rankSelect + 1) {
+            return true;
+        } else if (moving.rankMove == selected.rankSelect - 1) {
+            return true;
+        } else if ((moving.fileMove == selected.fileSelect + 1 && moving.rankMove == selected.rankSelect + 1)) {
+            return true;
+        } else if ((moving.fileMove == selected.fileSelect - 1 && moving.rankMove == selected.rankSelect - 1)) {
+            return true;
+        } else if ((moving.fileMove == selected.fileSelect - 1 && moving.rankMove == selected.rankSelect + 1)) {
+            return true;
+        } else if ((moving.fileMove == selected.fileSelect + 1 && moving.rankMove == selected.rankSelect - 1)) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
