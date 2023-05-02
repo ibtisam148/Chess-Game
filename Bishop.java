b922353b1374ae783e66c514125716324aba3e40
ibtisam148 <95739171+ibtisam148@users.noreply.github.com>
2023-05-02 19:22:08 +0500

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -1,20 +1,92 @@
 public class Bishop extends Piece {
     public Bishop(Colour colour) {
         super(colour);
-        if (colour == Colour.WHITE){
+        if (colour == Colour.WHITE) {
             name = 'B';
-        }else{
+        } else {
             name = 'b';
         }
     }
 
     @Override
-    public boolean Movement() {
-        return false;
+    public boolean MovementValidation(Selected selected, Moving moving, Moving[] list, Piece[][] board) {
+        int c, i;
+        boolean flag = false;
+        boolean flag1 = false;
+
+        for (i = 0; i < list.length; i++) {
+            if ((list[i].rankMove == moving.rankMove) && (list[i].fileMove == moving.fileMove)) {
+                flag1 = true;
+            }
+        }
+        if (!flag) {
+            System.out.print("Invalid move.\n");
+            return false;
+        }
+        if ((moving.fileMove < selected.fileSelect) && (moving.rankMove < selected.rankSelect)) {
+
+            i = selected.rankSelect - 1;
+            for (c = selected.fileSelect - 1; c > moving.fileMove; c--) {
+                if (board[c][i] != null) {
+                    flag = true;
+                }
+                i--;
+            }
+            if (flag == false) {
+                return true;
+            } else {
+                System.out.print("Invalid move.\n");
+                return false;
+            }
+        } else if ((moving.fileMove > selected.fileSelect) && (moving.rankMove > selected.rankSelect)) {
+
+            i = selected.rankSelect + 1;
+            for (c = selected.fileSelect + 1; c > moving.fileMove; c++) {
+                if (board[c][i] != null) {
+                    flag = true;
+                }
+                i++;
+            }
+            if (flag == false) {
+                return true;
+            } else {
+                System.out.print("Invalid move.\n");
+                return false;
+            }
+        } else if ((moving.fileMove < selected.fileSelect) && (moving.rankMove > selected.rankSelect)) {
+
+            i = selected.rankSelect + 1;
+            for (c = selected.fileSelect - 1; c > moving.fileMove; c--) {
+                if (board[c][i] != null) {
+                    flag = true;
+                }
+                i++;
+            }
+            if (flag == false) {
+                return true;
+            } else {
+                System.out.print("Invalid move.\n");
+                return false;
+            }
+        } else if ((moving.fileMove > selected.fileSelect) && (moving.rankMove < selected.rankSelect)) {
+
+            i = selected.rankSelect - 1;
+            for (c = selected.fileSelect + 1; c > moving.fileMove; c++) {
+                if (board[c][i] != null) {
+                    flag = true;
+                }
+                i--;
+            }
+            if (flag == false) {
+                return true;
+            } else {
+                System.out.print("Invalid move.\n");
+                return false;
+            }
+        } else {
+            System.out.print("Invalid move.\n");
+            return false;
+        }
     }
 
-    @Override
-    public boolean Validation() {
-        return false;
-    }
 }
