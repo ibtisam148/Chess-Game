bd0c54587cf3ac632f9e7964a6edeb8958b90ba0
ibtisam148 <95739171+ibtisam148@users.noreply.github.com>
2023-05-02 19:24:15 +0500

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,20 +1,40 @@
 public class Knight extends Piece {
     public Knight(Colour colour) {
         super(colour);
-        if (colour == Colour.WHITE){
+        if (colour == Colour.WHITE) {
             name = 'N';
-        }else{
+        } else {
             name = 'n';
         }
     }
 
     @Override
-    public boolean Movement() {
-        return false;
+    public boolean MovementValidation(Selected selected, Moving moving, Moving[] list, Piece board[][]) {
+        boolean flag = false;
+
+        for (int i = 0; i < list.length; i++) {
+            if ((list[i].rankMove == moving.rankMove) && (list[i].fileMove == moving.fileMove)) {
+                flag = true;
+            }
+        }
+        if (!flag) {
+            System.out.printf("Invalid move.\n");
+            return false;
+        }
+
+        if ((moving.rankMove == (selected.rankSelect - 2) && moving.fileMove == selected.fileSelect - 1) || (moving.rankMove == (selected.rankSelect - 2) && moving.fileMove == selected.fileSelect + 1)) {
+            return true;
+        } else if ((moving.rankMove == (selected.rankSelect + 2) && moving.fileMove == selected.fileSelect - 1) || (moving.rankMove == (selected.rankSelect + 2) && moving.fileMove == selected.fileSelect + 1)) {
+            return true;
+        } else if ((moving.fileMove == (selected.fileSelect + 2) && moving.rankMove == selected.rankSelect - 1) || (moving.fileMove == (selected.fileSelect + 2) && moving.rankMove == selected.rankSelect + 1)) {
+            return true;
+        } else if ((moving.fileMove == (selected.fileSelect - 2) && moving.rankMove == selected.rankSelect - 1) || (moving.fileMove == (selected.fileSelect - 2) && moving.rankMove == selected.rankSelect + 1)) {
+            return true;
+        } else {
+            System.out.printf("Invalid move.\n");
+            return false;
+        }
     }
 
-    @Override
-    public boolean Validation() {
-        return false;
-    }
+
 }
