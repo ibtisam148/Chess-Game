b2ae835005f2692d468968c27c5d1ac0f42b69dc
Hamza Javaid <77924744+hamzajd002@users.noreply.github.com>
2023-05-25 19:31:42 +0500

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,3 +1,9 @@
+/*
+This class represents an abstract class named "Piece" that 
+serves as a base class for different types of chess pieces.
+The class also has a constructor that takes a Colour parameter. 
+The constructor initializes the colour member variable with the provided color value.
+*/
 abstract public class Piece {
     Colour colour;
     char name = '-';
@@ -6,6 +12,10 @@ abstract public class Piece {
         this.colour = colour;
     }
 
-
+/*
+This method defines the behavior for movement validation of chess pieces. 
+It is declared as abstract, which means it does not provide an implementation 
+in this class but must be implemented by any concrete subclass extending the Piece class.
+*/
     public abstract boolean MovementValidation(Selected selected, Moving moving, Moving[] list, Piece board[][]);
 }
