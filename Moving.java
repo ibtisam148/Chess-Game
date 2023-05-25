8f9118da3498c5e30881eab813643fec7149edea
AyeshaT10 <109690535+AyeshaT10@users.noreply.github.com>
2023-05-25 14:01:42 +0500

    Update Moving.java

--- Moving.java
+++ Moving.java
@@ -1,3 +1,7 @@
+/*
+This file is related to moving
+the chess pieces
+*/
 public class Moving {
     int rankMove;
     int fileMove;
@@ -6,7 +10,7 @@ public class Moving {
         this.rankMove = rankMove - 65;
         this.fileMove = fileMove - 1;
     }
-
+//This function sets the rankMove and fileMove
     public Moving(int rankMove, int fileMove) {
         this.rankMove = rankMove;
         this.fileMove = fileMove;
